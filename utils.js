a0659a120b636d58169eb621147f873a3ca4a189
WarfarinBloodanger <85294649+WarfarinBloodanger@users.noreply.github.com>
2025-10-15 22:29:49 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -17,7 +17,7 @@ function getArgs(key) {
 document.addEventListener('DOMContentLoaded', function() {
     const currentPath = window.location.pathname;
     let scriptPath = currentPath.replace(/\.html$/, '.js');
-    if (scriptPath.endsWith(".js")) scriptPath += ".js";
+    if (!scriptPath.endsWith(".js")) scriptPath += ".js";
     const script = document.createElement('script');
     script.src = scriptPath;
     document.head.appendChild(script);
